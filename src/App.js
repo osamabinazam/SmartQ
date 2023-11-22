5e38e57fc7190021e460952409bb36ca9b7f6ae8
simranwaswani <simwni14@gmail.com>
2023-11-22 18:47:50 +0500

    This is the commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ThemePrimaryColor from './components/ThemePrimaryColor';
 
 // ----------------------------------------------------------------------
 
+// Main Function
 export default function App() {
   return (
     <ThemeConfig>
