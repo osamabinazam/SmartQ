4eb340e270aa0bfe75b7d397504d674fe31eebfb
Osama Bin Azam <osamabinazam@gmail.com>
2024-05-04 16:11:00 +0500

    Update exposing server configuration

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create(
   {
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://localhost:3002',
     headers: {
       'Content-Type': 'application/json',
     }
