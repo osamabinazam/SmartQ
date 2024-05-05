6ae3beda7e1b001a40e6e59d424c9cbee587f740
Osama Bin Azam <osamabinazam@gmail.com>
2024-05-05 19:36:04 +0500

    Add use information to No permission component

--- src/pages/Page401.js
+++ src/pages/Page401.js
@@ -11,6 +11,7 @@ import useAuth from 'src/hooks/useAuth';
 import {  useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import checkRole from 'src/utils/checkUserRole';
+import MainNavbar from '../layouts/main/MainNavbar';   
 
 // ----------------------------------------------------------------------
 
@@ -46,23 +47,33 @@ export default function Page401() {
 
 
   return (
-    <RootStyle title="404 Page Not Found | Minimal-UI">
+    <RootStyle title="401 Unauthorized Access " id="401">
       <Container>
+      <MainNavbar />
         <MotionContainer initial="initial" open>
           <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}>
             <motion.div variants={varBounceIn}>
               <Typography variant="h3" paragraph>
-                Sorry!, Unauhtorized Access
+               No Permission
               </Typography>
             </motion.div>
             <Typography sx={{ color: 'text.secondary' }}>
-              Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be sure to check
-              your spelling.
+              Sorry,  You do not have permission to view this page. Please use the SmartQ mobile app for customers or contact support if you believe this is an error.
             </Typography>
 
             <motion.div variants={varBounceIn}>
               <PageNotFoundIllustration sx={{ height: 260, my: { xs: 5, sm: 10 } }} />
             </motion.div>
+
+            {isAuthenticated && user && (
+              <Typography variant='h5' sx={{ mb: 2, color: 'text.secondary', alignSelf: 'flex-start' }}>
+                Logged in as: {user.username} ({user.usertype})
+              </Typography>
+            )}
+
+            <Button to="/" size="large" variant="contained" component={RouterLink} sx={{ mr: 2 }} >
+              Return Home
+            </Button>
 
             <Button to="/" size="large" variant="contained" onClick={handleLogout}>
               Logout
